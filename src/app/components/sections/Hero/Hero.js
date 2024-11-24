fadaee7ca4c736989e4bd6dbf5351c06307f9601
Joshua Williams <joshuawilliams1551@gmail.com>
2024-11-24 01:16:05 -0700

    Completed Hero Section

--- src/app/components/sections/Hero/Hero.js
+++ src/app/components/sections/Hero/Hero.js
@@ -4,17 +4,18 @@ import Image from "next/image";
 
 const Hero = (props) => {
   return (
-    <section id="hero" className="container">
+    <section id="hero" className="container-fluid pt-8">
       <div className="row hero__wrapper">
-        <div className="col-6 hero_column--left">
-          <h2 className="mb-6 hero_title">
-            Hi, my name is <span>Joshua</span>. A Software
-            <span>Developer</span>, with a passion for <span>Christ</span>.
+        <div className="col-5 hero_column--left">
+          <h2 className="mb-6 pt-6 hero_title">
+            Hi, my name is <span>Joshua</span>.<br />A Software
+            <span> Developer</span>,<br />
+            with a passion for <span>Christ</span>.
           </h2>
 
           <button className="hero_btn">Download Resume</button>
         </div>
-        <div className="col-6 hero_column--right">
+        <div className="col-4 hero_column--right">
           <div className="hero_img-wrapper">
             <img
               className="img-fluid"
