c9bc31c756022bb723e5702417aa391fee9ae529
Joshua Williams <Joshuawilliams1551@gmail.com>
2025-10-10 02:56:28 -0700

    Tested protfolio on dev and production

--- src/components/general/project_card.js
+++ src/components/general/project_card.js
@@ -2,7 +2,7 @@ export default function ProjectCard({ title, description, imageUrl, projectUrl }
     return (
         <div className="project-card">
             <div className="prjoect-card__content--wrapper pb-sm-5 pb-lg-4">
-                <img className="project-card__image img-fluid" src={imageUrl} loading="lazy" alt={`${title} Screenshot`} />
+                <img className="project-card__image img-fluid" src={imageUrl} alt={`${title} Screenshot`} />
                 <h3 className="project-card__title text--1 py-2 px-md-6 px-lg-4">{title}</h3>
                 <p className="project-card__description text--5 px-md-6 px-lg-4 is-hidden-touch">{description}</p>
             </div>
