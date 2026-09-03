d31b67fbd6678c020fade847ad26d43c39b8aa38
Jwill1551 <joshuawilliams1551@gmail.com>
2026-09-02 20:22:15 -0700

    Fixed an issue with site h1

--- src/components/sections/hero/hero.js
+++ src/components/sections/hero/hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
     return (
         <section id="hero" className="section columns is-multiline is-flex-tablet is-justify-content-center is-align-items-start portfolio-hero pt-sm-10 px-md-6 pt-md-8 pt-lg-12">
             <div className="column is-full is-two-fifths-desktop is-flex is-flex-align-items-start is-flex-direction-column">
-                <h1 class="portfolio-hero__heading has-text-centered-mobile has-text-white title--2 mb-5">Hi, my name is <span>Johsua</span>.</h1>
+                <h1 class="portfolio-hero__heading has-text-centered-mobile has-text-white title--2 mb-5">Hi, my name is <span>Joshua</span>.</h1>
                 <h2 className="portfolio-hero__heading has-text-centered-mobile has-text-white title--3 mb-2">Developing with <span>Excellence</span>, Guided by <span>Faith</span> and <span>Purpose</span>.</h2>
                 <p className="portfolio-hero__content has-text-left has-text-centered-mobile has-text-white title--5">
                     As a developer who believes in doing all things with integrity, I build software that not only works efficiently — but uplifts, inspires, and serves others.
